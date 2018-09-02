99782814c4ce2259de4a1de2c6a2415b31aeec7e
George <georgi.kasparyants@picsart.com>
2018-09-02 10:31:24 +0300

    sysctl multiple nested parameters

--- src/sysctl_module/sysctl_module.c
+++ src/sysctl_module/sysctl_module.c
@@ -12,7 +12,8 @@ MODULE_DESCRIPTION("A simple Linux driver");
 MODULE_VERSION("0.1");
 
 
-static int global_var = 1;
+static int global_var1 = 1;
+static int global_var2 = 1;
 static int min_val = 0;
 static int max_val = 5;
 static struct ctl_table_header* header;
@@ -20,21 +21,30 @@ static struct ctl_table_header* header;
 
 static struct ctl_table child_ctl_table[] = {
   {
-    .procname = "sample_value",
-    .data = &global_var,
+    .procname = "sample_value1",
+    .data = &global_var1,
     .maxlen = sizeof(int),
     .mode = 0644,
     .proc_handler = &proc_dointvec_minmax,
     .extra1 = &min_val,
     .extra2 = &max_val
   }, 
+  {
+    .procname = "sample_value2",
+    .data = &global_var2,
+    .maxlen = sizeof(int),
+    .mode = 0644,
+    .proc_handler = &proc_dointvec_minmax,
+    .extra1 = &min_val,
+    .extra2 = &max_val
+  },
   {}
 };
 
 
 static struct ctl_table parent_ctl_table[] = {
   {
-    .procname = "kernel",
+    .procname = "mykernel",
     .mode = 0555,
     .child = child_ctl_table
   }, 
@@ -47,14 +57,14 @@ static int __init sysctl_module_init(void) {
     printk(KERN_ALERT "Error: Failed to register parent_ctl_table\n");
     return -EFAULT;
   }
-  printk(KERN_INFO "Start global_var = %d\n", global_var);
+  printk(KERN_INFO "Start global_var1 = %d, global_var2 = %d\n", global_var1, global_var2);
   return SUCCESS;
 }
 
 
 static void __exit sysctl_module_exit(void) {
   unregister_sysctl_table(header);
-  printk(KERN_INFO "End global_var = %d\n", global_var);
+  printk(KERN_INFO "End global_var1 = %d, global_var2 = %d\n", global_var1, global_var2);
 }
 
 
